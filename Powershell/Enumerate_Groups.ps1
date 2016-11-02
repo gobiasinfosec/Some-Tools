724c5f762eeada98dd4df5a4d78aca55a4c6db31
Tobias Funke <thedavetheory@yahoo.com>
2016-11-02 09:37:57 -0400

    Add files via upload

---
+++ Powershell/Enumerate_Groups.ps1
@@ -0,0 +1,19 @@
+﻿import-module activedirectory
+
+#Will enumerate all the users in a group
+
+# optional, add a wild card..
+$GroupName = Read-Host "Enter the Group Name: "
+$outfile = Read-Host "Where to output: "
+
+$Groups = Get-ADGroup -filter {Name -like $GroupName} -Properties Name, Description | Select-Object Name, Description
+
+ForEach ($Group in $Groups)
+   {
+    $ArrayofMembers = Get-ADGroupMember -identity $($group.name) -recursive | Select-Object samaccountname, Name
+    foreach($Member in $ArrayofMembers)
+    {
+        Add-Content $outfile "`n `"$($group.name)`", `"$($group.Description)`",  `"$($Member.samaccountname)`",  `"$($Member.name)`" "
+    }
+
+}
