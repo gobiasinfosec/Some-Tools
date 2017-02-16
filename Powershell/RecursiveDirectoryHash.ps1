dfd684b5dafea7c90c2bdaf39e8f1887d69f8683
Tobias Funke <thedavetheory@yahoo.com>
2017-02-16 16:01:21 -0500

    Update RecursiveDirectoryHash.ps1

--- Powershell/RecursiveDirectoryHash.ps1
+++ Powershell/RecursiveDirectoryHash.ps1
@@ -1,10 +1,10 @@
 #Recursively hash a directory using built-in Windows tools
 #Outputs to a csv or xml file, comma delimited
 
-$myInput = Read-Host "Enter the directory you'd like to enumerate: "
-$myHashType = Read-Host "Enter the hashtype you would like to use: "
+$myInput = Read-Host "Enter the directory you'd like to enumerate"
+$myHashType = Read-Host "Enter the hashtype you would like to use"
 $myDownloads = Get-ChildItem -Recurse $myInput
-$Output = Read-Host "Enter the location where you would like to output(will be overwritten): "
+$Output = Read-Host "Enter the location where you would like to output(will be overwritten)"
 
 $fileNames = ForEach-Object -InputObject $myDownloads -Process {$_.FullName}
 $list = ''
